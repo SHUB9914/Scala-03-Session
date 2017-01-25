36b713e1de023fe6bec932943a37c07ac67ea1d3
SHUB9914 <shubhamagrawal6666@gmail.com>
2017-01-24 21:19:27 -0800

    Update SubSequence.scala

--- SubSequence.scala
+++ SubSequence.scala
@@ -1,3 +1,9 @@
+/**
+* Implement a method hasSubsequence for checking whether a List contains another List as a subsequence. 
+* For instance, List(1,2,3,4) would have List(1,2), List(2,3), and List(4) as subsequences. The signature of the method should be:
+* def hasSubsequence[A](list:List[A],sub:List[A]):Boolean
+*/
+
 class ListSubSequence
 {
   
